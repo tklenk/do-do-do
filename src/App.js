09deed30f134684287323393996d09ebe7209846
Tatiana <tekl@edc.dk>
2019-05-01 20:31:25 +0200

    render persons array in the state so this.state.persons using map function

--- src/App.js
+++ src/App.js
@@ -52,18 +52,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}/>
-          <Person
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')} 
-            changed={this.nameChangedHandler}/>
-            My hobbies: Racing
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/>
+          {this.state.persons.map(person => {
+            return <Person 
+              name={person.name} 
+              age={person.name} />
+          })}
         </div>
       )
     }
