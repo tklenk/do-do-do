8cd99d2ea89129f4bb77f0c0aeba713ce7d5d98c
Tatiana <tekl@edc.dk>
2019-06-18 15:38:39 +0200

    Setting and Updating state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       ],
       otherState: 'some other value',
       showPersons: false,
-      showCockpit: false,
+      showCockpit: true,
       changeCounter: 0
     }
   }
@@ -54,7 +54,13 @@ class App extends Component {
 
     const persons = [...this.state.persons]
     persons[personIndex] = person
-    this.setState({ persons: persons, changeCounter: this.state.changeCounter + 1 })
+
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1 
+      } 
+    })
   }
 
   deletePersonHandler = (personIndex) => {
